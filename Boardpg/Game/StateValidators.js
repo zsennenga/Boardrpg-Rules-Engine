6368756cd538a5bff6aaa74b9432edd545abb8b8
Zachary <Zachary@YossarianLives>
2014-05-14 01:44:01 -0700

    Skeleton of Event data

--- Boardpg/Game/StateValidators.js
+++ Boardpg/Game/StateValidators.js
@@ -2,4 +2,22 @@ function StateValidators(storage) {
 	this.db = storage;
 }
 
+
+/**
+ * Check that the data array has all the fields necessary
+ * 
+ * @param params
+ * @param data
+ * @returns {Boolean}
+ */
+StateValidators.prototype.checkSocketParams = function(params, data)	{
+	for (var param in params)	{
+		if (!(param in data))	{
+			return false;
+		}
+	}
+	
+	return true;
+};
+
 module.exports = StateValidators;
