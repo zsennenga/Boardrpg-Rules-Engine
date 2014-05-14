bad3650dc29a0f6270c9e93f6d3d6415e45a2f7e
Zachary <Zachary@YossarianLives>
2014-05-13 21:22:55 -0700

    Added inline documentation for the main socket handler

--- Boardpg/Server.js
+++ Boardpg/Server.js
@@ -1,49 +1,72 @@
 require('./Data/Config');
 require('./Data/State');
+
 var db = require('mysql2')
-  , io = require('socket.io')
-  , storage = require('./IO/DB')(db)
-  , eventData = require('./Data/EventData')
-  , stateValidators = require('./Game/StateValidators')(storage)
-  , eventHandlers = require('./Game/EventHandlers')(storage);
+, io = require('socket.io')
+, storage = require('./IO/DB')(db)
+, eventData = require('./Data/EventData')
+, stateValidators = require('./Game/StateValidators')(storage)
+, eventHandlers = require('./Game/EventHandlers')(storage);
 
 io.sockets.on('connection', function(socket) {
-	socket.on('gameState', function(data)	{
-		if (!('eventName' in data))	{
+	socket.on('gameState', function(data) {
+		/**
+		 * Check if an eventName was given
+		 */
+		if (!('eventName' in data)) {
 			socket.emit("NO_EVENT");
 			return;
 		}
 		
-		if (!(data.eventName in eventData))	{
+		/**
+		 * Checks if the event actually exists/has handlers
+		 */
+		if (!(data.eventName in eventData)) {
 			socket.emit("INVALID_EVENT");
 			return;
 		}
-		
+
 		var event = eventData[data.eventName];
 		var stateChecker = event.stateChecker;
 		var eventHandler = event.handler;
 		
-		if (!stateValidators.checkSocketParams(event.socketParams, data))	{
+		/**
+		 * Check if the socket data contains the parameters necessary to execute
+		 * the request
+		 */
+		if (!stateValidators.checkSocketParams(event.socketParams, data)) {
 			socket.emit("INVALID_PARAMS");
 			return;
 		}
 		
-		if (!(stateChecker in stateValidators))	{
+		/**
+		 * Checks if the statehandler for this event exists
+		 */
+		if (!(stateChecker in stateValidators)) {
 			socket.emit("NO_STATE_CHECKER");
 			return;
 		}
 		
-		if (!(eventHandler in eventHandlers))	{
+		/**
+		 * Check if the eventHandler exists
+		 */
+		if (!(eventHandler in eventHandlers)) {
 			socket.emit("NO_EVENT_HANDLER");
 			return;
 		}
 		
-		if (!stateValidators.stateChecker(data, socket))	{
+		/**
+		 * Validates the gamestate before executing the event handler
+		 */
+		if (!stateValidators.stateChecker(data, socket)) {
 			return;
 		}
 		
+		/**
+		 * Executes the eventhandler for the event
+		 */
 		eventHandlers.eventHandler.execute(data, socket);
-		
+
 	});
 });
 
